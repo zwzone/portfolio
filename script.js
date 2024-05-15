6433080fdea4ea864c27a4cd25e8f22092446039
Zakaria Wina <winazakaria0@gmail.com>
2024-05-15 16:27:37 +0100

    ADD: About section with Projects section

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const skillsTypes = ["languages", "frontend", "backend", "systems", "tools"];
+const skillsTypesElem = document.querySelector(".skills-types");
+const skillsTypesChildElems = skillsTypesElem.children;
+const skillsListElem = document.querySelector(".skills-list");
+const skillsListChildElems = skillsListElem.children;
+
+skillsTypesElem.addEventListener("click", skillToggler);
+
 function show() {
   const main = document.querySelector('main');
   if (main.style.filter) {
@@ -7,4 +15,22 @@ function show() {
   }
   document.querySelector('.humberger').classList.toggle('active');
   document.querySelector('nav').classList.toggle('active');
+}
+
+function skillToggler(e) {
+  const target = e.target;
+  for (let i = 0; i < skillsTypesChildElems.length; i++) {
+    if (skillsTypesChildElems[i].classList.contains("active")) {
+      skillsTypesChildElems[i].classList.remove("active");
+    }
+  }
+  target.classList.add("active");
+  for (let i = 0; i < skillsListChildElems.length; i++) {
+    if (skillsListChildElems[i].classList.contains("active")) {
+      skillsListChildElems[i].classList.remove("active");
+    }
+    if (skillsListChildElems[i].dataset.skillType === target.dataset.skillType) {
+      skillsListChildElems[i].classList.add("active");
+    }
+  }
 }
