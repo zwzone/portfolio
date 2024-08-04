2e7ee4eb21fffb2839ccf5b295e10ef20bfcae97
Zakaria Wina <winazakaria0@gmail.com>
2024-08-04 15:41:11 +0100

    ADD: Logo Showcase Animation in the entrance of the porfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const bodyElem = document.querySelector('body');
+const mainElem = document.querySelector('main');
+const humburgerElem = document.querySelector('.humburger');
+const navElem = document.querySelector('nav');
+
+const logoShowcaseElem = bodyElem.querySelector('.logo-showcase');
+const logoShowcaseImgElem = logoShowcaseElem.querySelector('img');
 const skillsTypes = ["languages", "frontend", "backend", "systems", "tools"];
 const skillsTypesElem = document.querySelector(".skills-types");
 const skillsTypesChildElems = skillsTypesElem.children;
@@ -5,12 +12,23 @@ const skillsListElem = document.querySelector(".skills-list");
 const skillsListChildElems = skillsListElem.children;
 const projectElems = document.querySelectorAll(".project");
 
-const bodyElem = document.querySelector('body');
-const mainElem = document.querySelector('main');
-const humburgerElem = document.querySelector('.humburger');
-const navElem = document.querySelector('nav');
-
-skillsTypesElem.addEventListener("click", skillToggler);
+// Logo Showcase Animation
+setTimeout(() => {
+  logoShowcaseImgElem.style.opacity = '1';
+  setTimeout(() => {
+    logoShowcaseImgElem.style.animation = 'rotation 1.5s';
+    setTimeout(() => {
+      logoShowcaseImgElem.style.filter = "drop-shadow(0px 0px 40px var(--color-primary-4))";
+      setTimeout(() => {
+        logoShowcaseElem.style.opacity = '0';
+        logoShowcaseImgElem.style.filter = "none";
+        setTimeout(() => {
+          logoShowcaseElem.style.display = 'none';
+        }, 400);
+      }, 800);
+    }, 1400);
+  }, 400);
+}, 400);
 
 function sideBarToggler() {
   if (mainElem.style.filter) {
@@ -28,7 +46,7 @@ function sideBarRemove() {
   navElem.classList.remove('active');
 }
 
-function skillToggler(e) {
+skillsTypesElem.addEventListener("click", (e) => {
   const target = e.target;
   for (let i = 0; i < skillsTypesChildElems.length; i++) {
     if (skillsTypesChildElems[i].classList.contains("active")) {
@@ -44,7 +62,7 @@ function skillToggler(e) {
       skillsListChildElems[i].classList.add("active");
     }
   }
-}
+});
 
 // Hide Header on scroll down
 let prevScrollpos = window.scrollY;
